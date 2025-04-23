40c5061d8b3aaabb85bdb5bdcef3f6d41ccd1eaf
felixreichenbach <felix.reichenbach@viam.com>
2025-04-23 16:09:25 +0200

    dynamic credentials

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-import '../app.css';
-import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
-import { ViamProvider } from '$lib';
-import type { Snippet } from 'svelte';
-import { dialConfigs } from './configs';
-import Parts from './components/parts.svelte';
+  import "../app.css";
+  import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools";
+  import { ViamProvider } from "$lib";
+  import type { Snippet } from "svelte";
+  import Parts from "./components/parts.svelte";
+  import type { DialConf } from "@viamrobotics/sdk";
 
-interface Props {
-  children: Snippet;
-}
+  interface Props {
+    children: Snippet;
+    data: Record<string, DialConf>;
+  }
 
-let { children }: Props = $props();
+  let { data, children }: Props = $props();
+  data["255bbeb9-6231-49a1-b265-41ae796bacdc"]["host"] =
+    "mac-felix-main.9e2zv86qpa.viam.cloud";
 </script>
 
-<ViamProvider {dialConfigs}>
+<ViamProvider dialConfigs={data}>
   <Parts />
   {@render children()}
   <SvelteQueryDevtools />
--- my-app/src/routes/+layout.ts
+++ my-app/src/routes/+layout.ts
@@ -1,1 +1,12 @@
 export const prerender = true;
+
+import type { DialConf } from "@viamrobotics/sdk";
+import type { LayoutLoad } from "./$types";
+
+export const load: LayoutLoad = async ({ fetch }) => {
+  // TODO: Port ist static which is not ideal
+  const res = await fetch(`http://localhost:33332/data.json`);
+  const data = await res.json();
+  const dialConfig: Record<string, DialConf> = data as Record<string, DialConf>;
+  return dialConfig;
+};
