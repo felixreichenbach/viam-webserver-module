3fe44d97465a925f3f7239ff99e41ff67e78a933
felixreichenbach <felix.reichenbach@viam.com>
2025-04-24 20:38:27 +0200

    styling added

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -15,7 +15,12 @@
 </script>
 
 <ViamProvider dialConfigs={data}>
-  <Camera partID={Object.keys(data)[0]} name="camera" />
-  {@render children()}
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold">HPE Sealant Check</h1>
+    <div class="my-4"></div>
+    <Camera partID={Object.keys(data)[0]} name="camera" />
+    {@render children()}
+  </div>
+
   <SvelteQueryDevtools />
 </ViamProvider>
--- my-app/src/routes/components/camera.svelte
+++ my-app/src/routes/components/camera.svelte
@@ -40,6 +40,20 @@
 {#if query.current.error}
   {query.current.error.message}
 {:else}
-  <img {src} alt="" width="200" />
-  <button onclick={handleButtonClick}>Click Me</button>
+  <div class="flex flex-row gap-4 m-4">
+    <div class="basis-400"><img {src} alt="" width="700" /></div>
+    <div class="basis-1/3">
+      <div class="flex flex-col items-center justify-center gap-4 h-full">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-[50px] px-[100px] mb-20 rounded"
+          onclick={handleButtonClick}>Refresh</button
+        >
+
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-[50px] px-[100px] rounded"
+          onclick={handleButtonClick}>Accept</button
+        >
+      </div>
+    </div>
+  </div>
 {/if}
