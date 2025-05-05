27efa5f734256ea0bec8a3b10fe418dd757df90a
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-05-05 22:24:46 +0200

    webapp optimized structure (#12)

--- webserver/web-app/src/routes/components/vision.svelte
+++ webserver/web-app/src/routes/components/vision.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-  import {
-    createResourceClient,
-    createResourceQuery,
-    useConnectionStatus,
-  } from "$lib";
-  import { Struct, VisionClient } from "@viamrobotics/sdk";
-  import Metrics from "./metrics.svelte";
+  import { createResourceClient, createResourceQuery } from "$lib";
+  import VisionControl from "$lib/vision-control.svelte";
+  import VisionData from "$lib/vision-data.svelte";
+  import { VisionClient } from "@viamrobotics/sdk";
 
   interface Props {
     partID: string;
@@ -20,9 +17,6 @@
     () => partID,
     () => name
   );
-
-  const status = useConnectionStatus(() => partID);
-  $inspect(status.current);
 
   const queryOptions = $state({
     refetchInterval: 1000,
@@ -52,12 +46,12 @@
       : undefined
   );
 
-  let extra = $derived(
-    query.current.data?.extra ? query.current.data.extra.clone() : new Struct()
+  const extra = $derived(
+    query.current.data ? JSON.stringify(query.current.data.extra, null, 2) : ""
   );
 
   function handleCheckContour() {
-    query.current.refetch().then((result) => {
+    query.current.refetch().then(() => {
       console.log("contour refreshed");
     });
   }
@@ -67,60 +61,18 @@
   }
 </script>
 
-<!-- Main Content Area -->
-{#if status.current === "disconnected"}
-  <div class="flex flex-col items-center justify-center min-h-screen">
-    <h1 class="text-2xl font-bold">Disconnected</h1>
-    <p class="text-lg">Please connect to the robot.</p>
-  </div>
-{:else if status.current === "connecting"}
-  <div class="flex flex-col items-center justify-center min-h-screen">
-    <h1 class="text-2xl font-bold">Connecting...</h1>
-  </div>
-{:else if status.current === "connected"}
-  {#if !query.current.error}
-    <div class="flex border-0 border-red-500 overflow-auto">
-      <div class="border-0">
-        <!--column left -->
-        <img
-          {src}
-          alt=""
-          class="object-scale-down max-w-full max-h-full w-full h-auto"
-        />
-      </div>
-      <div class="flex-col border-0 p-4 w-1/3">
-        <!--column right -->
-        <div class="flex flex-nowrap border-0 items-center justify-between">
-          <h1 class="text-2xl font-bold mt-12 mb-10 mx-auto">Sealant Check</h1>
-          <button
-            class="bg-blue-200 hover:bg-blue-500 text-xl text-white py-[10px] px-[10px] rounded ml-auto self-center"
-            onclick={() => location.reload()}
-          >
-            Reload
-          </button>
-        </div>
-        <div
-          class="flex flex-col border-0 border-purple-500 items-center justify-center min-h-[100px]"
-        >
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-xl text-white font-bold py-[50px] px-[100px] mb-10 rounded"
-            onclick={handleCheckContour}>Check</button
-          >
-          <div class="flex-grow"></div>
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-xl text-white font-bold py-[50px] px-[100px] mb-10 rounded"
-            onclick={handleAccept}>Accept</button
-          >
-        </div>
-        <div
-          class="flex flex-col border-0 border-purple-500 items-center justify-center"
-        >
-          <!--column right row 2 -->
-          <Metrics data={extra} />
-        </div>
+{#if query.current.error}
+  {query.current.error.message}
+{:else}
+  <div class="grid grid-cols-2 min-w-[400px] border-0 border-red-500">
+    <div class="flex flex-col border-0 border-amber-300">
+      <div class=""><img {src} alt="" width="700" /></div>
+    </div>
+    <div class="flex flex-col border-2 border-purple-300">
+      <div class="flex flex-col items-center justify-center gap-4 h-full">
+        <VisionControl {handleCheckContour} {handleAccept} />
+        <VisionData data={extra} />
       </div>
     </div>
-  {:else}
-    {query.current.error.message}
-  {/if}
+  </div>
 {/if}
