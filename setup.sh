16aa0b2208eb4bbf7aa1f9bdef48eb410469850f
felixreichenbach <felix.reichenbach@viam.com>
2025-05-12 20:47:21 +0200

    node version added

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ if ! command -v node >/dev/null 2>&1; then
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     nvm install node
+elif [ "$(node -v | sed 's/^v//')" \< "18.18.0" ]; then
+    echo "Node.js version is less than 18.18. Proceeding with update..."
+    nvm install node
+    nvm alias default node
 else
     echo "Node.js is already installed. Skipping installation."
     exit 0
