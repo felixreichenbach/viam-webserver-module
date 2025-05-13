378082a3e7e4f93c8e951fd03bf07284cb8e18ea
felixreichenbach <felix.reichenbach@viam.com>
2025-05-13 18:52:09 +0200

    tailwind styles properly applied

--- src/lib/vision-data.svelte
+++ src/lib/vision-data.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
-  export let data: string = "";
+  interface Props {
+    data: any;
+  }
+
+  let { data }: Props = $props();
+
+  let resultContours = $state("good");
+  let resultArcLength = $state("bad");
+  let resultArea = $state("");
+
+  function changeAreaColor(color: string) {
+    if (resultArea === "bad") {
+      resultArea = "good";
+    } else {
+      resultArea = "bad";
+    }
+  }
 </script>
 
 <div>
-  <pre>{data}</pre>
+  <div id="arclength" class="result {resultArcLength} mb-4">
+    <span class="text-3xl font-bold">Arc Length</span>
+  </div>
+  <div id="area" class="result {resultArea} mb-4">
+    <span class="text-3xl flex justify-center items-center font-bold">Area</span
+    >
+  </div>
+  <div id="contours" class="result {resultContours}">
+    <span class="text-3xl font-bold">Contours</span>
+  </div>
 </div>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  .result {
+    background-color: rgb(255, 255, 255);
+    @apply w-60 h-25 border-2 flex justify-center items-center rounded;
+  }
+  .result.bad {
+    background-color: rgb(255, 0, 0);
+  }
+  .result.good {
+    background-color: rgb(0, 255, 0);
+  }
+</style>
