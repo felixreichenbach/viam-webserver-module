0461df2741a8a7933e3c08822e65cc442d1a2a2b
felixreichenbach <felix.reichenbach@viam.com>
2025-05-13 10:09:02 +0200

    working webserver

--- cmd/module/cmd.go
+++ cmd/module/cmd.go
@@ -3,9 +3,9 @@ package main
 import (
 	webserver "github.com/felixreichenbach/web-app-module"
 
+	"go.viam.com/rdk/components/generic"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
-	"go.viam.com/rdk/services/generic"
 )
 
 func main() {
