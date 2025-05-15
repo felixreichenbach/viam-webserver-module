dbf60d51c40665a4ce20a802e2fcc01893f2aafb
felixreichenbach <felix.reichenbach@viam.com>
2025-05-15 15:16:19 +0200

    restructured code

--- src/lib/vision-data.svelte
+++ src/lib/vision-data.svelte
@@ -5,6 +5,7 @@
     type PointCloudObject,
     type Struct,
   } from "@viamrobotics/sdk";
+  import { checkContours, type Contour } from "$lib/contours";
 
   interface Props {
     data:
@@ -18,12 +19,6 @@
       | undefined;
   }
 
-  type Contour = {
-    arclength: number;
-    area: number;
-    hausdorff?: { [key: string]: number };
-  };
-
   type Extra = {
     ref_contours: Contour[];
     contours: Contour[];
@@ -31,51 +26,6 @@
 
   let props: Props = $props();
   let extra = $derived(props.data?.extra?.toJson() as Extra | undefined);
-
-  function checkContours(ref_contours: Contour[], contours: Contour[]) {
-    let result = { length: "", area: "", shape: "" };
-    if (ref_contours && ref_contours.length == contours.length) {
-      for (const [rci, refcont] of ref_contours.entries()) {
-        for (const [ci, contour] of contours.entries()) {
-          console.log(
-            "diff length",
-            ci,
-            Math.abs(refcont.arclength - contour.arclength)
-          );
-          if (Math.abs(refcont.arclength - contour.arclength) < 100) {
-            result["length"] = "good";
-          }
-          console.log("diff area", Math.abs(refcont.area - contour.area));
-          if (Math.abs(refcont.area - contour.area) < 100) {
-            result["area"] = "good";
-          }
-          if (contour.hausdorff) {
-            console.log("hausdorff", typeof contour.hausdorff);
-            const keys = Object.keys(contour.hausdorff);
-            keys.forEach((key) => {
-              const hausdorffValue = contour.hausdorff
-                ? contour.hausdorff[key]
-                : undefined;
-              console.log("diff hausdorff", hausdorffValue);
-              if (hausdorffValue !== undefined && hausdorffValue < 100) {
-                result["shape"] = "good";
-              }
-            });
-          }
-          if (result["shape"] === "") {
-            result["shape"] = "bad";
-          }
-        }
-        if (result["length"] === "") {
-          result["length"] = "bad";
-        }
-        if (result["area"] === "") {
-          result["area"] = "bad";
-        }
-      }
-    }
-    return result;
-  }
 
   let result = $derived(
     checkContours(extra?.ref_contours ?? [], extra?.contours ?? [])
