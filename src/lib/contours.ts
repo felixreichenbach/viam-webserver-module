4387a04c4c8525a44a34284156c6bbcab650445e
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-05-16 17:45:34 +0200

    Tag Image for data manager upload (#20)
    * restructured code
    
    * tagimage working

---
+++ src/lib/contours.ts
@@ -0,0 +1,50 @@
+export type Contour = {
+  arclength: number;
+  area: number;
+  hausdorff?: { [key: string]: number };
+};
+
+export const checkContours = (ref_contours: Contour[], contours: Contour[]) => {
+  let result = { length: "", area: "", shape: "" };
+  if (ref_contours && ref_contours.length == contours.length) {
+    for (const [rci, refcont] of ref_contours.entries()) {
+      for (const [ci, contour] of contours.entries()) {
+        console.log(
+          "diff length",
+          ci,
+          Math.abs(refcont.arclength - contour.arclength)
+        );
+        if (Math.abs(refcont.arclength - contour.arclength) < 100) {
+          result["length"] = "good";
+        }
+        console.log("diff area", Math.abs(refcont.area - contour.area));
+        if (Math.abs(refcont.area - contour.area) < 100) {
+          result["area"] = "good";
+        }
+        if (contour.hausdorff) {
+          console.log("hausdorff", typeof contour.hausdorff);
+          const keys = Object.keys(contour.hausdorff);
+          keys.forEach((key) => {
+            const hausdorffValue = contour.hausdorff
+              ? contour.hausdorff[key]
+              : undefined;
+            console.log("diff hausdorff", hausdorffValue);
+            if (hausdorffValue !== undefined && hausdorffValue < 100) {
+              result["shape"] = "good";
+            }
+          });
+        }
+        if (result["shape"] === "") {
+          result["shape"] = "bad";
+        }
+      }
+      if (result["length"] === "") {
+        result["length"] = "bad";
+      }
+      if (result["area"] === "") {
+        result["area"] = "bad";
+      }
+    }
+  }
+  return result;
+};
